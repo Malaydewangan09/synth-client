9facc3cd03c56045616f745baad683cdd5cdaa36
Malaydewangan09 <malaydewangan310@gmail.com>
2025-02-01 23:11:59 +0530

    feat: add router and search imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from "react"
 import { useState } from "react"
+import { useRouter, useSearchParams } from 'next/navigation'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
 import { FcGoogle } from "react-icons/fc"
